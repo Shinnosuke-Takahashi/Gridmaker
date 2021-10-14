c85a36c9ee6fd3cecdfd0b354fd7330d88077bd2
Shinnosuke-Takahashi <shinnosuketakahashisemail@gmail.com>
2021-10-14 15:10:27 -0400

    comments edited, fillU() if statement updated

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ let numCols = 0;
 let colorSelected;
 let grid = document.getElementById("grid");
 
-
-/*
-things to consider:
-NEED TO ADD COLOR FUNCTIONALITY
-
-throw exception for remove and add rows/cols when grid is empty
-*/
-
 //Add a row
 function addR() {
     //alert("Clicked Add Row");
@@ -113,7 +105,7 @@ function fillU(){
     //alert("Clicked Fill All Uncolored")
     for (let i = 0; i < numRows; i++) {
         for (let j = 0; j < numCols; j++) {
-            if (grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor === "") {
+            if (grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor === "" || grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor === "white") {
                 grid.querySelectorAll("tr")[i].querySelectorAll("td")[j].style.backgroundColor = colorSelected;
             }
         }
